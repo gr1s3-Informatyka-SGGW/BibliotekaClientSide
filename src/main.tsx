1e243a4f39e8906330bce5deb4ec05a457bb7d9d
Szymon <szymon.credo@gmail.com>
2026-01-14 21:41:36 +0100

    Ups

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,6 @@ import ProfileInfoPanel from "./general_elements/ProfileInfoPanel.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <ProfileInfoPanel info={{name: "Jan", surname: "Cydejko", email: "Jan.cydejko@email.com", credit_card_number: '5786'}}/>
+        <App/>
     </StrictMode>
 )
