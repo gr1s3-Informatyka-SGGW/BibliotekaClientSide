0d099331af97c7398b7bedc94784982ee2bc197f
Natalia <bardadynnatalia225@gmail.com>
2026-01-15 17:03:35 +0100

    poprawiony wyglad i struktura

--- src/elements_user/UserProfileView.tsx
+++ src/elements_user/UserProfileView.tsx
@@ -1,4 +1,3 @@
-
 import React, { useContext } from "react";
 import NavSidebar from "../general_elements/NavSidebar.tsx";
 import ProfileInfoPanel from '../general_elements/ProfileInfoPanel';
@@ -31,7 +30,7 @@ export default function UserProfileView() {
                 flexDirection: 'column',
                 width: '100%',
                 boxSizing: 'border-box',
-                paddingRight: '0.15em'
+                padding: '2em',
             }}>
                 {/* 1. Nagłówek: Ikona i Tytuł */}
                 <div style={{
@@ -39,14 +38,18 @@ export default function UserProfileView() {
                     textDecoration: 'none',
                     color: '#631433',
                     transition: 'color 0.3s',
-                    marginRight: '0.15em',
                     alignItems: 'center',
                     gap: '0.5em'
                 }}>
                     <h1 style={{
                         fontSize: '2em',
+                        marginTop: '0',
                         marginBottom: '0.5em',
-                        fontWeight: 'bold'
+                        fontWeight: 'bold',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        gap: '0.5em'
                     }}>
                         <img
                             src={accountCircleIcon}
@@ -60,11 +63,14 @@ export default function UserProfileView() {
                 <div style={{
                     display: 'flex',
                     flexDirection: 'row',
-                    width: '100%'
+                    flexWrap: 'wrap',
+                    width: '100%',
+                    gap: '2em',
+                    alignItems: 'flex-start'
                 }}>
 
                     {/* LEWA KOLUMNA: Panel profilu */}
-                    <div style={{ flex: '0 0 auto', width: '25em' }}>
+                    <div style={{ flex: '0 0 auto', width: '22em', marginTop: '0.5em' }}>
                         <ProfileInfoPanel info={{
                             name: "Jan",
                             surname: "Kowalski",
@@ -77,13 +83,11 @@ export default function UserProfileView() {
                     <div style={{
                         flex: '1',
                         display: 'flex',
-                        flexDirection: 'column',
-                        width: '100%',
+                        flexDirection: 'column'
                     }}>
                         <ProfileBookList
                             header="Zarezerwowane książki"
                             icon={ribbonIcon}
-                            count={userReservations.length}
                         >
                             {userReservations.map(res => (
                                 <ReservationComponent key={res.book.book_id} info={res} />
@@ -93,7 +97,6 @@ export default function UserProfileView() {
                         <ProfileBookList
                             header="Wypożyczone książki"
                             icon={bookIcon}
-                            count={userRents.length}
                         >
                             {userRents.map(rent => (
                                 <RentComponent key={rent.book.book_id} info={rent} />
@@ -111,32 +114,42 @@ export default function UserProfileView() {
  */
 function getRent(): Rent {
     return {
-        rent_id: 1,
         book: {
             book_id: 101,
             title: "Władca Pierścieni: Drużyna Pierścienia",
             authors: ["J.R.R. Tolkien"],
-            isbn: "978-83-7298-953-6"
+            isbn_number: "978-83-7298-953-6",
+            publish_year: 1954,
+            publisher: "George Allen & Unwin",
+            genre: ["Fantasy", "Przygoda"],
+            language: "Polski",
+            length: 423,
+            keywords: ["Pierścień", "Hobbit"]
         },
-        rent_date: "2025-12-01",
-        return_date: "2026-01-15"
+        borrow_date: new Date("2025-01-01"),
+        return_date: new Date("2026-01-21")
     };
 }
+
+
 
 /**
  * Funkcja generująca przykładowe dane rezerwacji.
  */
 function getRes(): Reservation {
     return {
-        reservation_id: 1,
         book: {
             book_id: 202,
             title: "Hobbit, czyli tam i z powrotem",
             authors: ["J.R.R. Tolkien"],
-            isbn: "978-83-244-0308-0"
+            isbn_number: "978-83-244-0308-0",
+            publish_year: 1937,
+            publisher: "SuperNowa",
+            genre: ["Fantasy"],
+            language: "Polski",
+            length: 310,
+            keywords: ["Smok", "Bilbo"]
         },
-        reservation_date: "2025-12-20",
-        reserve_to: "2026-01-10",
-        status: "Oczekująca"
+        reserve_to: new Date("2026-01-21"),
     };
 }
